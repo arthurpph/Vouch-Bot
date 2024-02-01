2c672ce4cfec6bb939d4535211de9f588653946c
arthurpph <arthurpiana13@gmail.com>
2024-02-01 01:47:18 -0300

    better code readability

--- commands.py
+++ commands.py
@@ -1,6 +1,9 @@
 import discord
 from discord import app_commands, User, Forbidden, Embed
 from discord.ext import commands
+
+import utils
+from utils import check_permission, check_permission_only_staff
 from main import div_roles
 
 from config import Config
@@ -84,7 +87,7 @@ class Commands(commands.Cog):
         await ctx.response.send_message(embed=embed, ephemeral=True)
 
         guild = ctx.guild
-        if check_promotion(guild, usuario.id):
+        if utils.check_promotion(guild, usuario.id):
             await promote_player(guild, usuario)
 
     @app_commands.command(name="delete_vouch", description="Deleta um vouch de um jogador")
@@ -231,7 +234,7 @@ class Commands(commands.Cog):
                 pass
 
             await channel.send(embed=Embed(color=discord.Color.blue(), title="Promoção",
-                                       description=f"{ctx.user.mention} promoveu {member.mention} para {role.mention}!"))
+                                           description=f"{ctx.user.mention} promoveu {member.mention} para {role.mention}!"))
         except InsufficientPermission as e:
             embed = Embed(description=f"Erro: {e}")
             await ctx.response.send_message(embed=embed, ephemeral=True)
@@ -274,23 +277,7 @@ class Commands(commands.Cog):
                 return
 
             global role
-            role_name = convert_role_to_name(guild.get_member(usuario.id))
-            if not role_name:
-                await ctx.response.send_message(
-                    embed=Embed(color=discord.Color.blue(), description="Role name inválido"), ephemeral=True)
-                return
-
-            if role_name == "Divisao 1":
-                role = Config.get_div_role_id(2)
-            elif role_name == "Divisao 2":
-                role = Config.get_div_role_id(3)
-            elif role_name == "Divisao 3":
-                role = Config.get_casual_role_id()
-            elif role_name == "Casual":
-                await ctx.response.send_message(
-                    embed=Embed(color=discord.Color.blue(), description=f"{usuario.mention} já está no rank Casual"),
-                    ephemeral=True)
-                return
+            role = utils.get_previous_role(ctx, usuario)
 
             guild_role = guild.get_role(role)
             if not guild_role:
@@ -310,49 +297,6 @@ class Commands(commands.Cog):
             await ctx.response.send_message(
                 embed=Embed(color=discord.Color.blue(), description="Não tenho permissão para executar isso"),
                 ephemeral=True)
-
-
-def check_permission(ctx):
-    div_council_roles_id = Config.get_div_council_roles_id()
-    staff_roles = Config.get_staff_roles_id()
-
-    for role in ctx.user.roles:
-        if any(role.id in list for list in [div_council_roles_id, staff_roles]):
-            return True
-
-    return False
-
-
-def check_permission_only_staff(ctx):
-    staff_roles = Config.get_staff_roles_id()
-
-    for role in ctx.user.roles:
-        for staff_role_id in staff_roles:
-            if role.id == staff_role_id:
-                return True
-
-    return False
-
-
-def get_minimum_promotion_vouches(guild: discord.Guild, user_id: int):
-    role_name = convert_role_to_name(guild.get_member(user_id))
-    if role_name == "Divisao 2":
-        return 2
-    elif role_name == "Divisao 3":
-        return 4
-    elif role_name == "Casual":
-        return 5
-    else:
-        return None
-
-
-def check_promotion(guild: discord.Guild, user_id: int):
-    user_vouches = Vouch.get_user_vouches(user_id)
-    minimum_promotion = get_minimum_promotion_vouches(guild, user_id)
-    if user_vouches and minimum_promotion is not None and len(user_vouches.keys()) >= minimum_promotion:
-        return True
-
-    return False
 
 
 async def setup(bot):
--- utils.py
+++ utils.py
@@ -98,3 +98,65 @@ async def promote_player(guild: discord.Guild, member: Member):
 
     await channel.send(embed=Embed(color=discord.Color.blue(), title="Promoção",
                                    description=f"{member.mention} foi promovido automaticamente para {role.mention}!"))
+
+
+async def get_previous_role(ctx, user):
+    role_name = convert_role_to_name(ctx.guild.get_member(user.id))
+    if not role_name:
+        await ctx.response.send_message(
+            embed=Embed(color=discord.Color.blue(), description="Role name inválido"), ephemeral=True)
+        return
+
+    if role_name == "Divisao 1":
+        return Config.get_div_role_id(2)
+    elif role_name == "Divisao 2":
+        return Config.get_div_role_id(3)
+    elif role_name == "Divisao 3":
+        return Config.get_casual_role_id()
+
+    await ctx.response.send_message(
+        embed=Embed(color=discord.Color.blue(), description=f"{user.mention} já está no rank Casual"),
+        ephemeral=True)
+
+
+def check_permission(ctx):
+    div_council_roles_id = Config.get_div_council_roles_id()
+    staff_roles = Config.get_staff_roles_id()
+
+    for role in ctx.user.roles:
+        if any(role.id in list for list in [div_council_roles_id, staff_roles]):
+            return True
+
+    return False
+
+
+def check_permission_only_staff(ctx):
+    staff_roles = Config.get_staff_roles_id()
+
+    for role in ctx.user.roles:
+        for staff_role_id in staff_roles:
+            if role.id == staff_role_id:
+                return True
+
+    return False
+
+
+def get_minimum_promotion_vouches(guild: discord.Guild, user_id: int):
+    role_name = convert_role_to_name(guild.get_member(user_id))
+    if role_name == "Divisao 2":
+        return 2
+    elif role_name == "Divisao 3":
+        return 4
+    elif role_name == "Casual":
+        return 5
+    else:
+        return None
+
+
+def check_promotion(guild: discord.Guild, user_id: int):
+    user_vouches = vouch.Vouch.get_user_vouches(user_id)
+    minimum_promotion = get_minimum_promotion_vouches(guild, user_id)
+    if user_vouches and minimum_promotion is not None and len(user_vouches.keys()) >= minimum_promotion:
+        return True
+
+    return False
