98817bfa2588e3b60d72d7177a3b7a6b67eaabb6
arthurpph <arthurpiana13@gmail.com>
2024-02-08 01:51:25 -0300

    added bot_has_permissions annotation and exception handling

--- commands.py
+++ commands.py
@@ -41,8 +41,10 @@ class Commands(commands.Cog):
             return
 
         guild = ctx.guild
-
         div_council_roles = Config.get_div_council_roles_id()
+        council_duels_id = Config.get_council_duels_role_id()
+        council_duels_role = guild.get_role(council_duels_id)
+
         allowed = False
         for role_2 in ctx.user.roles:
             if role_2.id in div_council_roles:
@@ -55,8 +57,6 @@ class Commands(commands.Cog):
                 ephemeral=True)
             return
 
-        council_duels_id = Config.get_council_duels_role_id()
-        council_duels_role = guild.get_role(council_duels_id)
         if not council_duels_role:
             await ctx.response.send_message(
                 embed=Embed(color=discord.Color.blue(), description="Cargo council duels inválido"), ephemeral=True)
@@ -67,6 +67,7 @@ class Commands(commands.Cog):
             f"{ctx.user.mention} está disponível para duelos de vouch {modo.name} {council_duels_role.mention}!")
 
     @app_commands.command(name="give_vouch", description="Atribui um vouch a um usuário")
+    @app_commands.checks.bot_has_permissions(manage_roles=True)
     @app_commands.describe(usuario="Escolha o usuário", descricao="Adicione uma descrição")
     async def give_vouch(self, ctx: commands.Context, usuario: User, descricao: str):
         self.bot.logger.info(f"Command: /give_vouch {usuario} {descricao} by {ctx.user.name}")
@@ -169,6 +170,7 @@ class Commands(commands.Cog):
         await ctx.response.send_message(embed=embed)
 
     @app_commands.command(name="promote", description="Promove um usuário para uma divisão")
+    @app_commands.checks.bot_has_permissions(manage_roles=True)
     @app_commands.describe(usuario="Escolha um usuário", rank="Escolha um rank")
     @app_commands.choices(rank=[
         app_commands.Choice(name="Divisão 1", value=1),
@@ -186,68 +188,51 @@ class Commands(commands.Cog):
 
         guild = ctx.guild
         member = await guild.fetch_member(usuario.id)
+        role_name_formatted = rank.name.replace("ã", "a")
+        role = guild.get_role(convert_name_to_role(role_name_formatted))
         channel = guild.get_channel(int(Config.get_promotions_channel_id()))
+        casual_role = guild.get_role(int(Config.get_casual_role_id()))
+
+        if not guild.me.guild_permissions.manage_roles:
+            raise InsufficientPermission("Não tenho permissão para gerenciar cargos")
+
+        if not channel:
+            raise InvalidChannelId("ID do canal de promoções é inválido")
+
+        if not role:
+            raise InvalidRole("Cargo da divisão é inválido")
+
+        if role in member.roles:
+            embed = Embed(color=discord.Color.blue(), description=f"{member.mention} já está nesta divisão")
+            await ctx.response.send_message(embed=embed, ephemeral=True)
+            return
+
+        if not casual_role:
+            raise InvalidRole("Cargo 'casual' é inválido")
+
+        for member_role in member.roles:
+            if member_role.id in div_roles:
+                await member.remove_roles(member_role)
+
+        if casual_role in member.roles:
+            await member.remove_roles(casual_role)
+
+        await member.add_roles(role)
+
+        embed = Embed(color=discord.Color.blue(), description="Usuário promovido com sucesso!")
+        await ctx.response.send_message(embed=embed, ephemeral=True)
 
         try:
-            if not guild.me.guild_permissions.manage_roles:
-                raise InsufficientPermission("Não tenho permissão para gerenciar cargos")
+            await usuario.send(
+                embed=Embed(color=role.color, description=f"Parabéns! Você foi promovido para {rank.name}!"))
+        except Forbidden:
+            pass
 
-            if not channel:
-                raise InvalidChannelId("ID do canal de promoções é inválido")
-
-            role_name_formatted = rank.name.replace("ã", "a")
-            role = guild.get_role(convert_name_to_role(role_name_formatted))
-
-            if not role:
-                raise InvalidRole("Cargo da divisão é inválido")
-
-            if role in member.roles:
-                embed = Embed(color=discord.Color.blue(), description=f"{member.mention} já está nesta divisão")
-                await ctx.response.send_message(embed=embed, ephemeral=True)
-                return
-
-            casual_role = guild.get_role(int(Config.get_casual_role_id()))
-
-            if not casual_role:
-                raise InvalidRole("Cargo 'casual' é inválido")
-
-            try:
-                for member_role in member.roles:
-                    if member_role.id in div_roles:
-                        await member.remove_roles(member_role)
-
-                if casual_role in member.roles:
-                    await member.remove_roles(casual_role)
-
-                await member.add_roles(role)
-            except Forbidden:
-                await ctx.response.send_message(
-                    embed=Embed(color=discord.Color.blue(), description="Eu não tenho permissão para editar cargos."),
-                    ephemeral=True)
-                return
-
-            embed = Embed(color=discord.Color.blue(), description="Usuário promovido com sucesso!")
-            await ctx.response.send_message(embed=embed, ephemeral=True)
-
-            try:
-                await usuario.send(
-                    embed=Embed(color=role.color, description=f"Parabéns! Você foi promovido para {rank.name}!"))
-            except Forbidden:
-                pass
-
-            await channel.send(embed=Embed(color=discord.Color.blue(), title="Promoção",
-                                           description=f"{ctx.user.mention} promoveu {member.mention} para {role.mention}!"))
-        except InsufficientPermission as e:
-            embed = Embed(description=f"Erro: {e}")
-            await ctx.response.send_message(embed=embed, ephemeral=True)
-        except InvalidChannelId as e:
-            embed = Embed(description=f"Erro: {e}")
-            await ctx.response.send_message(embed=embed, ephemeral=True)
-        except InvalidRole as e:
-            embed = Embed(description=f"Erro: {e}")
-            await ctx.response.send_message(embed=embed, ephemeral=True)
+        await channel.send(embed=Embed(color=discord.Color.blue(), title="Promoção",
+                                       description=f"{ctx.user.mention} promoveu {member.mention} para {role.mention}!"))
 
     @app_commands.command(name="purge", description="Rebaixa um usuário")
+    @app_commands.checks.has_permissions(manage_roles=True)
     @app_commands.describe(usuario="Escolha o usuário")
     async def purge(self, ctx: commands.Context, usuario: User):
         self.bot.logger.info(f"Command: /purge {usuario} by {ctx.user.name}")
@@ -260,44 +245,33 @@ class Commands(commands.Cog):
 
         guild = ctx.guild
         member = await guild.fetch_member(usuario.id)
+        role = await utils.get_previous_role(ctx, usuario)
+        guild_role = guild.get_role(role)
         casual = guild.get_role(int(Config.get_casual_role_id()))
-
-        if not guild.me.guild_permissions.manage_roles:
-            embed = Embed(color=discord.Color.blue(), description="Não tenho permissão para alterar cargos")
-            await ctx.response.send_message(embed=embed, ephemeral=True)
-            return
 
         if not member:
             embed = Embed(color=discord.Color.blue(), description="Usuário não encontrado no servidor")
             await ctx.response.send_message(embed=embed, ephemeral=True)
             return
 
-        try:
-            if not casual:
-                embed = Embed(color=discord.Color.blue(), description="Cargo 'casual' não existe")
-                await ctx.response.send_message(embed=embed, ephemeral=True)
-                return
+        if not casual:
+            embed = Embed(color=discord.Color.blue(), description="Cargo 'casual' não existe")
+            await ctx.response.send_message(embed=embed, ephemeral=True)
+            return
 
-            role = await utils.get_previous_role(ctx, usuario)
-
-            guild_role = guild.get_role(role)
-            if not guild_role:
-                await ctx.response.send_message(
-                    embed=Embed(color=discord.Color.blue(), description="Cargo não encontrado no servidor"),
-                    ephemeral=True)
-                return
-
-            for role in member.roles:
-                if role.id in div_roles:
-                    await member.remove_roles(role)
-
-            await member.add_roles(guild_role)
+        if not guild_role:
             await ctx.response.send_message(
-                embed=Embed(color=discord.Color.blue(), description="Usuário rebaixado com sucesso"), ephemeral=True)
-        except Forbidden:
-            await ctx.response.send_message(
-                embed=Embed(color=discord.Color.blue(), description="Não tenho permissão para executar isso"),
+                embed=Embed(color=discord.Color.blue(), description="Cargo não encontrado no servidor"),
                 ephemeral=True)
+            return
+
+        for role in member.roles:
+            if role.id in div_roles:
+                await member.remove_roles(role)
+
+        await member.add_roles(guild_role)
+        await ctx.response.send_message(
+            embed=Embed(color=discord.Color.blue(), description="Usuário rebaixado com sucesso"), ephemeral=True)
 
     @app_commands.command(name="purge_list", description="Cria um tópico para votação")
     @app_commands.describe(usuario="Escolha o usuário")
@@ -324,12 +298,10 @@ class Commands(commands.Cog):
 
         member_div_int = int(member_div.split(" ")[1])
         div_purge_forum_id = Config.get_div_purge_forum_id(member_div_int)
-
-        if not div_purge_forum_id:
-            raise InvalidRoleId("Purge forum ID inválido")
-
         forum_channel = guild.get_channel(div_purge_forum_id)
         div_council_role = guild.get_role(Config.get_div_council_role_id(member_div_int))
+        if not div_purge_forum_id:
+            raise InvalidRoleId("Purge forum ID inválido")
 
         if not member:
             await ctx.followup.send(
@@ -393,8 +365,11 @@ class Commands(commands.Cog):
         except Exception:
             pass
 
-        await ctx.followup.send(embed=Embed(color=discord.Color.blue(), title="Erro",
-                                            description=f"{error.original}\n\n Por favor reporte para shauuu\_"))
+        if isinstance(error, app_commands.BotMissingPermissions):
+            await ctx.followup.send(embed=Embed(color=discord.Color.blue(), title="Erro", description=f"Eu não tenho a(s) permissão(ões): {', '.join(error.missing_permissions)}"))
+        else:
+            await ctx.followup.send(embed=Embed(color=discord.Color.blue(), title="Erro",
+                                                description=f"{error.original}\n\n Por favor reporte para shauuu\_"))
 
 
 async def setup(bot):
